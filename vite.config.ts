c77f0bede4f83fe6d53aab32ad1cdfe1e454f523
mjcadz <mjcadzdev@gmail.com>
2022-04-16 13:23:16 +0930

    :seedling: feat(config): vite lint errors + src alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [vue(), eslintPlugin()],
 })
