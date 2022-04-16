dd0d631637cb50be4a945d50afa5127ad36af003
mjcadz <mjcadzdev@gmail.com>
2022-04-16 13:58:17 +0930

    :seedling: feat(formatting): tailwind prettier plugin

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="bg-red-500 p-4 pt-2 text-3xl font-bold underline">Hello world!</h1>
 </template>
